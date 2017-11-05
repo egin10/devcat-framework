d0997b151f3a94696a0cc380a1dc56111b22836a
egin10 <wweusz@gmail.com>
2017-11-06 02:15:30 +0700

    rename Core->App

--- app/init.php
+++ app/init.php
@@ -1,11 +1,11 @@
 <?php
 
 spl_autoload_register(function($class){
-    require_once 'Core/'.$class.'.php';
+    require_once 'App/'.$class.'.php';
+
+    //Eloquent
+    require_once "../vendor/autoload.php";
+
+    //Database
+    require_once "App/Database.php";
 });
-
-//Eloquent
-require_once "../vendor/autoload.php";
-
-//Database
-require_once "Core/Database.php";
